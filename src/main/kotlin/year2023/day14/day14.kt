10c9b493cd3e3a385a8058cc08b52f1c8336ee51
sean madden <sdm350@gmail.com>
2023-12-15 09:43:18 -0800

    day14 empty

---
+++ src/main/kotlin/year2023/day14/day14.kt
@@ -0,0 +1,31 @@
+package year2023.day14
+
+import java.util.Deque
+import java.util.Queue
+
+val testInput = """
+""".trimIndent()
+
+val puzzleInput = """
+""".trimIndent()
+
+val part2TestInput = """
+""".trimIndent()
+
+val test2Input = """
+""".trimIndent()
+
+fun part1() {
+}
+
+fun part2(values: List<Long>) {
+}
+
+fun main(args: Array<String>) {
+
+    val startTime = System.currentTimeMillis()
+
+
+    val endTime = System.currentTimeMillis()
+    println("millis elapsed ${endTime - startTime}")
+}
--- src/main/kotlin/year2023/day15/day15.kt
+++ src/main/kotlin/year2023/day15/day15.kt
@@ -1,8 +1,5 @@
 package year2023.day15
 
-import java.util.Deque
-import java.util.Queue
-
 val testInput = """
     rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
 """.trimIndent()
